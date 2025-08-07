3b719c7accd0d281d92ec478432df19ce3d795dd
Prithwin Ratnan <prithwinratnan55@gmail.com>
2025-08-07 23:15:17 +0530

    [feat] completed structured output

--- structured_output/with_structured_op.py
+++ structured_output/with_structured_op.py
@@ -1,6 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict,Annotated,Optional,Literal
 load_dotenv()
 
 llm= ChatGoogleGenerativeAI(
@@ -12,10 +12,16 @@ llm= ChatGoogleGenerativeAI(
 # Schema
 class Review(TypedDict): 
     summary:str
-    sentiment:str
+    sentiment:Literal["pos","neg"] #To provice selction from options
 
 structured_output=llm.with_structured_output(Review)
 result=structured_output.invoke("The Hyundai Creta 2025 continues to impress with its bold design, spacious interiors, and feature-packed cabin. The 1.5L turbo petrol engine offers smooth performance, while the suspension handles city and highway drives comfortably. The panoramic sunroof, large infotainment screen, and ADAS features add a premium feel.")
 print(result)
-print(result.get("sentiment"))
+# print(result.get("sentiment"))
 print(result["sentiment"])
+
+"""
+Disadvantage: although we specify the datas structure its not necessary that llm outputs the same data
+the data is not valildated
+
+"""
